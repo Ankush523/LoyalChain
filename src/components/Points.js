ac052c7de578f7970ec050bb4c70d1489b6ffeb6
Arch0125 <dasarchisman25@gmail.com>
2022-05-03 19:23:58 +0530

    Point program creator side complete

--- src/components/Points.js
+++ src/components/Points.js
@@ -7,6 +7,7 @@ import useContract from '../hooks/useContract';
 
 function Points() {
     const[reward,setReward]=useState('');
+    const[prgname,setPrgname]=useState('');
     const[point,setPoint]=useState('');
     const[currentAccount,setcurrentAccount]=useState('');
     const[balance,setBalance]=useState('');
@@ -33,7 +34,7 @@ function Points() {
     }
 
     const addPoints = () =>{
-        Pointcontract.addPoint('abc',reward,point);
+        Pointcontract.addPoint(prgname,reward,point);
     }
 
 
@@ -45,6 +46,10 @@ function Points() {
             <label className='subtitle'>Connected Account : <span className='display'>{acc}</span></label>
             <label className='subtitle'>Balance : <span className='display'>{balance} MATIC</span></label>
             <div className='optionbox' >
+               <div className='options'>
+                   <label>Program Name :</label>
+                   <input className='prginput' placeholder='Eg. 10x Points Program' onChange={e => setPrgname(e.target.value)}></input>
+               </div>
                <div className='options' >
                     <label>Rewards per MATIC spent</label>
                     <input id='points' className='smallinput' placeholder='0.05 MATIC' onChange={e => setReward(e.target.value)} ></input>
@@ -54,10 +59,8 @@ function Points() {
                     <input className='smallinput' placeholder='x10000' onChange={e => setPoint(e.target.value)} ></input>
                </div>
             </div>
-            <label className='example' ><span className='extitle' >Example<br/></span>If a customer spends 100 MATIC, they will get {reward * point} points which is redeemable for {reward * point / 100} MATIC</label>
-            <a><button className='buybtn'>Start Program</button></a>
-            <button onClick={showPoint}>Show Point</button>
-            <button onClick={addPoints} >Add Point</button>
+            <label className='example' ><span className='extitle' >Explanation <br/></span>If a customer spends 1 MATIC, they will get {reward * point} points which is redeemable for {reward * point / 100} MATIC</label>
+            <a><button onClick={addPoints} className='buybtn'>Start Program</button></a>
         </div>
      );
 }
