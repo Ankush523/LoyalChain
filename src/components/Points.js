1b96d498e38129e5004e38c27d12d3f8d4d7a63c
Arch0125 <dasarchisman25@gmail.com>
2022-05-03 17:48:11 +0530

    Add web3 auth

--- src/components/Points.js
+++ src/components/Points.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Buyget.css'
+import { ethers } from 'ethers';
 import {BsChevronLeft} from "react-icons/bs"
 import { calculateNewValue } from '@testing-library/user-event/dist/utils';
+import useProvider from '../hooks/useProvider';
 
 function Points() {
     const[reward,setReward]=useState('');
     const[point,setPoint]=useState('');
+    const[currentAccount,setcurrentAccount]=useState('');
+    const[balance,setBalance]=useState('');
+    const provider=useProvider();
+
+    useEffect(() => {  
+     window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then(result => {
+          setcurrentAccount(result[0]);
+  
+          provider.getBalance(result[0])
+            .then(ethers.utils.formatEther)
+            .then(setBalance);
+
+        });  
+    }, [])
+
+    const acc = currentAccount.substring(0,5)+'...'+currentAccount.substring(39,42);
+
 
     return ( 
         <div className='main'>
-            
             <a href='/Campaign'><label id='darktext' className='subtitle'><span className='icon'><BsChevronLeft/></span> All programs</label></a>
             <br/>
             <label className='title'>Setup Points Program</label>
+            <label className='subtitle'>Connected Account : <span className='display'>{acc}</span></label>
+            <label className='subtitle'>Balance : <span className='display'>{balance} MATIC</span></label>
             <div className='optionbox' >
                <div className='options' >
                     <label>Rewards per MATIC spent</label>
