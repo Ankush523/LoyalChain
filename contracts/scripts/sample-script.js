011c4f355f5009559a729497fe94cdbd0045aa90
Arch0125 <dasarchisman25@gmail.com>
2022-05-03 18:16:28 +0530

    Contract Deployed

--- contracts/scripts/sample-script.js
+++ contracts/scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const PointProgram = await hre.ethers.getContractFactory("PointProgram");
+  const pointprogram = await PointProgram.deploy();
 
-  await greeter.deployed();
+  await pointprogram.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("PointProgram deployed to:", pointprogram.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ src/contracts.js
@@ -0,0 +1,7 @@
+import PointProgramABI from '../contracts/artifacts/contracts/PointProgram.sol/PointProgram.json'
+
+const contracts = {
+  'PointProgram': { abi: PointProgramABI.abi, address: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0' }
+};
+
+export default contracts
