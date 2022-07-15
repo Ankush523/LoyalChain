09bc6f5ef0a9019d0ea5f4a92c197b634b56db0f
route-2 <ruthurao@gmail.com>
2022-07-16 02:10:59 +0530

    update

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -2,8 +2,8 @@ import contracts from "../contracts";
 import { ethers } from "ethers"
 
 function useContract(contractName) {
-  const tempProvider = new ethers.providers.Web3Provider(window.ethereum);
-  const tempSigner = tempProvider.getSigner();
+  const tempProviderr = new ethers.providers.Web3Provider(window.ethereum);
+  const tempSigner = tempProviderr.getSigner();
 
   const contract = new ethers.Contract(contracts[contractName].address, contracts[contractName].abi, tempSigner);
 
